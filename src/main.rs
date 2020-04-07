db5dd4bf2843bffadb1e6a4e4ec6c2e414304762
maitland_j <maitland_j@hotmail.com>
2020-04-07 12:00:28 +0100

    Added a unit test

--- src/main.rs
+++ src/main.rs
@@ -5,3 +5,19 @@ fn add_two_number(first : i32, second : i32) -> i32 {
 fn main() {
     println!("Adding: {} + {} = {}", 1, 2, add_two_number(1, 2));
 }
+
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        
+        let first = 1;
+        let second = 2;
+
+        assert_eq!(3, add_two_number(first, second))
+    }
+}
